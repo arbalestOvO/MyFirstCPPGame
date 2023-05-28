b58094b5be9c42d5575134475924060e0799e571
arbalestOvO <195712388@qq.com>
2023-05-28 18:06:28 +0800

    测试功能，并且添加了自定义的Debug头文件

---
+++ Source/MyFirstArpgGame/DebugMacros.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include<DrawDebugHelpers.h>
+
+#define DebugPoint(Location) if(GetWorld())  DrawDebugPoint(GetWorld(), Location, 12, FColor::Black, true);
+#define DebugVector(_Vector) UE_LOG(LogTemp, Warning, TEXT("This Vector is X : %lf, Y : %lf, Z : %lf"), _Vector.X, _Vector.Y, _Vector.Z);
+#define DebugDouble(_double) UE_LOG(LogTemp, Warning, TEXT("%lf"), _double);
+#define DebugString(_String) UE_LOG(LogTemp, Warning, TEXT(_String), _String);
---
+++ Source/MyFirstArpgGame/Test_Character.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Test_Character.h"
+#include<MyFirstArpgGame/DebugMacros.h>
+#include "GameFramework/CharacterMovementComponent.h"
+// Sets default values
+ATest_Character::ATest_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATest_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	//GetCharacterMovement()->SetSimulatePhysics(false);
+	//GetCharacterMovement()->Physics
+}
+
+// Called every frame
+void ATest_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	RunningTime += DeltaTime;
+	DrawDebugSphere(GetWorld(), GetActorLocation(), 100.f, 24, FColor::Red, false, -1);
+	//const auto Ff = FVector(GetActorForwardVector().X, GetActorForwardVector().Y, FMath::Sin(RunningTime) * 10) * 50 * DeltaTime;
+	AddActorWorldOffset(GetActorForwardVector() * 500 * DeltaTime);
+	AddActorWorldOffset(FVector(0,0,FMath::Sin(RunningTime) * 50));
+}
+
+// Called to bind functionality to input
+void ATest_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
